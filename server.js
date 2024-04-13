be1e869fb90e3fb76f20b88cc6a45c5d426eab9b
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-04-13 13:06:05 -0600

    Basic express setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Pet Box';
+
+app.get('/', (request, response) => {
+  response.send('Oh hey Pet Box');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
