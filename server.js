1ed34afa5e1a0a047d4b5e17188853dada4c8ffe
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-04-13 14:42:40 -0600

    Change local port of express server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 8080);
 app.locals.title = "StackPedia API";
 
 // GET HOME
