9fa8b160c1e5a131ceb94163d412f04cf0bf5c4a
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-05-11 01:22:38 -0600

    Add endpoint to POST user login status to database with email and password validation

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
 const User = require("./models/User");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: "3d" })
+}
 
 app.set("port", process.env.PORT || 8080);
 app.locals.title = "StackPedia API";
@@ -112,7 +117,26 @@ app.post("/api/v1/user/signup", async (request, response) => {
 
   try {
     const user = await User.signup(email, password)
-    response.status(200).json({ email, user })
+
+    //create token
+    const token = createToken(user._id)
+    
+    response.status(200).json({ email, token })
+  } catch(error) {
+    response.status(400).json({ error: error.message })
+  }
+})
+
+app.post("/api/v1/user/login", async (request, response) => {
+  const { email, password } = request.body;
+
+  try {
+    const user = await User.login(email, password)
+
+    //create token
+    const token = createToken(user._id)
+
+    response.status(200).json({ email, token })
   } catch(error) {
     response.status(400).json({ error: error.message })
   }
